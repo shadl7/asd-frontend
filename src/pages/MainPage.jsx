193c499c196ac75577306ae70ea60c4365d2250d
Shadl7 <admin@shadl7.ru>
2023-11-19 19:35:20 +0700

    Add links

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div>
+            <a href={"https://shadl7.ru/Client-Shadl7.exe"} download={""}>Скачать клиент</a>
+            <br/>
+            <a href={"https://git.shadl7.ru/cgit"}>Исходный код</a>
         </div>
     );
 };
